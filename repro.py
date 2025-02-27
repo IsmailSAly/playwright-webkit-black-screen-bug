ac0d137b330c7a4f3feb6014b2a7fc017441672e
ialy <ialy@appareo.com>
2025-02-27 10:48:51 -0600

    using playwright devices

--- repro.py
+++ repro.py
@@ -3,13 +3,11 @@ from playwright.sync_api import sync_playwright
 def reproduce_bug():
     with sync_playwright() as p:
         browser = p.webkit.launch(headless=False)
-        context = browser.new_context(
-            viewport={"width": 375, "height": 812},
-            is_mobile=True  # This causes WebKit to turn black
-        )
+        ipad_gen7 = p.devices["iPad (gen 7)"]
+        context = browser.new_context(**ipad_gen7)
         page = context.new_page()
         page.goto("https://playwright.com")
-	page.wait_for_timeout(10000)
+        page.wait_for_timeout(10000)
         browser.close()
 
 if __name__ == "__main__":
