ad40e2eb9980b32ad93ed9c54773fd5012e99ff4
ialy <ialy@appareo.com>
2025-02-27 11:01:47 -0600

    added logging

--- repro.py
+++ repro.py
@@ -6,6 +6,11 @@ def reproduce_bug():
         ipad_gen7 = p.devices["iPad (gen 7)"]
         context = browser.new_context(**ipad_gen7)
         page = context.new_page()
+        
+        page.on("console", lambda msg: print(f"Console Log [{msg.type()}]: {msg.text()}"))
+        page.on("pageerror", lambda error: print(f"Page Error: {error}"))
+        browser.on("disconnected", lambda: print("⚠️ Browser Disconnected"))
+
         page.goto("https://playwright.com")
         page.wait_for_timeout(10000)
         browser.close()
